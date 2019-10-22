be28143212a9c642fccdd2fff92175f402896306
LeslauD <36820890+LeslauD@users.noreply.github.com>
2019-10-22 21:27:30 +0800

    Update Visuliza.py

--- Visuliza.py
+++ Visuliza.py
@@ -1,24 +1,27 @@
-#---------------------------Fit Plot--------------------------------------------------------------------------
-def Fit_plot(history, batch_size, epoch):
+def Fit_plot(history, batch_size, epoch ,model):
+    #visuliza acc,loss
     iters = range(1,epoch+1)
     plt.figure()
     # acc
-    plt.plot(iters, history.history['accuracy'], 'r', label='train acc')
+    plt.plot(iters, history.history['acc'], 'r', label='train acc')
     # loss
     plt.plot(iters, history.history['loss'], 'g', label='train loss')
-        # val_acc
-    plt.plot(iters, history.history['val_accuracy'], 'b', label='val acc')
-        # val_loss
+    #dice
+    plt.plot(iters, history.history['dice'], 'y', label='train dice')
+    # val_acc
+    plt.plot(iters, history.history['val_acc'], 'b', label='val acc')
+    # val_loss
     plt.plot(iters, history.history['val_loss'], 'k', label='val loss')
+    #val_dice
+    plt.plot(iters, history.history['val_dice'], 'm', label='val dice')
     plt.grid(True)
     plt.xlabel("epoch")
     plt.ylim(0,1.1)
-    plt.ylabel('acc-loss')
+    plt.ylabel('acc-loss-dice')
     plt.legend(loc="best")
-    plt.title('Metrics (batch_size : '+str(batch_size)+' epoch : '+str(epoch)+' )')       
-    plt.savefig('./Visuliza_Metrics.png')
+    plt.title(model+' : '+'Metrics (batch_size : '+str(batch_size)+' epoch : '+str(epoch)+' )')       
+    plt.savefig('../result/1D/vessel/Visuliza_Metrics_'+model+'.png')
     plt.show()
-
 
 
 
@@ -44,17 +47,17 @@ def dice_plot(Test_Mask,pred,model):
 
 
 
-def pred_plot(raw, lable, pred, order=-1):
+def pred_plot(raw, lable, pred,model,order=-1):
+    #raw_img , label_img , pred_img ,  model_name,index of img
     if order==-1:
-        order=int(len(raw)/2)
+        order=int(len(raw)/2)   #default index=25
     raw_ = raw[order]
     pred_ = pred[order]
     lable_ = lable[order]
-    dice_ = (lable_ ^ bina(pred_).astype('int32'))
+    dice_ = (lable_ ^ bina(pred_).astype('int32')) #将pred与label进行XOR运算，以判断pred与label的差别
     
     print('Dice of raw['+str(order)+']:',dice_coef(lable_,pred_))
-    
-    fig = plt.figure(figsize=(15,15),facecolor='red')
+    fig = plt.figure(figsize=(10,10),facecolor='red')
     raw = fig.add_subplot(221)
     lable = fig.add_subplot(222)
     pred = fig.add_subplot(223)
@@ -75,6 +78,8 @@ def pred_plot(raw, lable, pred, order=-1):
     dice.imshow(dice_,cmap="gray")
     dice.axis('off')
     dice.set_title('XOR Iamge')
+    plt.title(model+': '+'Pred Plot')
+    plt.savefig('../result/1D/vessel/Pred_Plot_'+model+'.png')
 
 
 
