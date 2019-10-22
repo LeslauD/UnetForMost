662744ab0810adfcfcb767991d32521ecd1a7e4f
LeslauD <36820890+LeslauD@users.noreply.github.com>
2019-10-22 17:25:52 +0800

    Update dataProcess.py

--- dataProcess.py
+++ dataProcess.py
@@ -72,12 +72,25 @@ def CSplMask(mask):
     return soma,vess
 
 
-def dice_coef(y_true, y_pred,smooth=1):  
-    #值越大越好（0,1）
-    y_true_f = y_true.flatten()#将img_array一维化
+#img array in   | img array out
+#Erode (腐蚀) Dilate (膨胀)  Change
+def EDC(img):
+    img = img.astype('uint8')
+    kernel = np.ones((5,5),np.uint8)
+    imgE = cv.erode(img, kernel, iterations = 2)  #对原图进行两次腐蚀
+    imgD = cv.dilate(img, kernel, iterations = 2) #对原图进行两次膨胀
+    
+    return imgE,imgD
+
+
+
+def dice_coef(y_true, y_pred,smooth=500*500*0.01):  
+    y_trueE,y_trueD = EDC(y_true)
+    y_trueE_f = y_trueE.flatten()
     y_pred_f = y_pred.flatten()
-    intersection = np.sum(y_true_f * y_pred_f)#|X∩Y|
-    dice=(2 * intersection + smooth) / (np.sum(y_true_f*y_true_f) + np.sum(y_pred_f*y_pred_f) + smooth)  #（2*|X∩Y|）/（|X|+|Y|）  2*重叠区域大小/总的大小
+    intersection = np.sum(y_trueE_f * y_pred_f)
+    y_trueED=y_trueE+1-y_trueD
+    dice=(2 * intersection + smooth) / ( np.sum(y_trueE_f) + np.sum( y_pred_f*y_trueED.flatten() ) +smooth )
     return dice
 
 def dice(y_true, y_pred,smooth=1):  
@@ -95,12 +108,6 @@ def diceloss(y_true, y_pred,smooth=1):
     return 1-dice(y_true, y_pred,smooth=1)
 
 
-#img array in   | img array out
-#Erode (腐蚀) Dilate (膨胀)  Change
-def EDC(img):
-    img = img.astype('uint8')
-    kernel = np.ones((5,5),np.uint8)
-    imgE = cv.erode(img, kernel, iterations = 2)  #对原图进行两次腐蚀
-    imgD = cv.dilate(img, kernel, iterations = 2) #对原图进行两次膨胀
-    
-    return imgD
+
+
+
