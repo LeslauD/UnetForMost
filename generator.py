65f080011acd7f1aed73427c62bd2f182097b254
LeslauD <36820890+LeslauD@users.noreply.github.com>
2019-11-12 16:25:34 +0800

    Delete generator.py

--- generator.py
+++
@@ -1,6 +0,0 @@
-#def data generator
-def imgen():
-    gen = ImageDataGenerator(horizontal_flip=True,
-                             vertical_flip=True,
-                             fill_mode='wrap',)
-    return gen
